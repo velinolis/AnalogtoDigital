11c44a750f00163cc771e046b4b2070933a63171
velinolis <velinolis@gmail.com>
2021-04-06 14:28:02 +0200

    Update CustomUse.ino

--- CustomUse.ino
+++ CustomUse.ino
@@ -2,9 +2,11 @@
 AnalogtoDigital AD;
 
 void setup() {
-  AD.Convert_All();
-  //AD.N_Pins(2);
-  //AD.Select_Pin(14,0,1,1);
+  
+  AD.Convert_All();    
+//AD.Select_Pin(14,0,1,1);    
+//AD.N_Pins(2);   
+  
 }
 
 void loop() {}
