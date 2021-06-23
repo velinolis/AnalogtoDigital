fb9182cc0c3c83461bc4cb62a55193f9a1564cec
velinolis <velinolis@gmail.com>
2021-06-23 12:12:01 +0200

    Update AnalogtoDigital.cpp

--- AnalogtoDigital.cpp
+++ AnalogtoDigital.cpp
@@ -1,25 +1,15 @@
 /*
-########################################################
-AnalogtoDigital library
-
-Convert analog pins A0-A3 to digital pins, 
-excluded I2C pins A4 and A5
+---------------------------
+# AnalogtoDigital library #
+---------------------------
 
   pin A0 = 14
   pin A1 = 15
   pin A2 = 16
   pin A3 = 17
-  pin A4 = 18 --> I2C SDA
-  pin A5 = 19 --> I2C SCL
+  pin A4 = 18 (excluded) --> I2C SDA
+  pin A5 = 19 (excluded) --> I2C SCL
   
-Convert_All()
-Convert all pins (A0-14, A1-15, A2-16, A3-17)
-Select_Pin()
-Select pin to convert (0/1/14, 0/1/15, 0/1/16, 0/1/17),
-0-Analog/1-Digital/14-15-16-17-Digital
-N_Pins()     
-Number of pins to convert (1-4)
-########################################################
 */
 
 #include "AnalogtoDigital.h"
